9094135180935820726838acd14bde9be1d7376f
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-01-23 19:30:42 +0100

    Save local version before overwrite

--- BackEnd/BookLogApi/obj/Debug/net8.0/BookLogApi.AssemblyInfo.cs
+++ BackEnd/BookLogApi/obj/Debug/net8.0/BookLogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d140b86dc4cefb5b974b3b3babead7006b199b02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d839414b59fc03c9ba17849024a8eabe366bd71")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
