5057b18500d8c1b4edc6134cdfce20119b1d553a
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-01-31 07:26:07 +0100

    updating the button for geners and preaping to add funcationality for drop down, search for geners when author adds or edit books

--- BackEnd/BookLogApi/obj/Debug/net8.0/BookLogApi.AssemblyInfo.cs
+++ BackEnd/BookLogApi/obj/Debug/net8.0/BookLogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8bc561888d3eda36b893de45100e3e08493090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1a1552dc6d0cd79c5be2229ce7aed7a7184c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
